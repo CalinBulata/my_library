3b7d5003e671f8297b11320aa789152bb7e82cb3
Calin Bulata <calin-teodor.bulata@kcl.ac.uk>
2025-10-31 23:16:11 +0000

    Added static configuration for pythonanywhere

--- my_library/urls.py
+++ my_library/urls.py
@@ -31,4 +31,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
-urlpatterns += static(settings.STATIC_URL)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
